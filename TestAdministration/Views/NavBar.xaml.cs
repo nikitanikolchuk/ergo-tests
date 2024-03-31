ee54ba5a2ca09cef1f3946acd2e54792a7050f1f
Nikita Nikolchuk <nikolnik@fit.cvut.cz>
2024-03-31 14:00:56 +0200

    Add links to manuals

--- TestAdministration/Views/NavBar.xaml.cs
+++ TestAdministration/Views/NavBar.xaml.cs
@@ -1,9 +1,25 @@
+using System.Diagnostics;
+using System.Windows;
+
 namespace TestAdministration.Views;
 
 public partial class NavBar
 {
+    private const string TextManualsLink = "https://rehabilitace.lf1.cuni.cz/publikacni-cinnost-uvod";
+    private const string VideoManualsLink = "https://kurzy.lf1.cuni.cz/";
+
     public NavBar()
     {
         InitializeComponent();
+    }
+
+    private void OnTextManualsClick(object sender, RoutedEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo(TextManualsLink) { UseShellExecute = true });
+    }
+
+    private void OnVideoManualsClick(object sender, RoutedEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo(VideoManualsLink) { UseShellExecute = true });
     }
 }
